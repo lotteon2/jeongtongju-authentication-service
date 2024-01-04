db90c71263b1cdee6cd117399a5b6dd705d68b68
woo yup Lee <zjadlspun1114@naver.com>
2024-01-04 14:40:46 +0900

    FIX 소셜 로그인 시, 이미 탈퇴한 회원은 해당 로그인 불가능하게 버그 수정

---
+++ src/main/java/com/jeontongju/authentication/exception/AlreadyWithdrawalMemberException.java
@@ -0,0 +1,8 @@
+package com.jeontongju.authentication.exception;
+
+public class AlreadyWithdrawalMemberException extends RuntimeException {
+
+  public AlreadyWithdrawalMemberException(String msg) {
+    super(msg);
+  }
+}
